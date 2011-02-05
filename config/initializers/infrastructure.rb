f9c84df01e7c4cce18b32229eab0cb15e4af849c
Luismi Cavalle <luismi@lmcavalle.com>
2011-02-05 18:15:19 +0100

    InProcess is cheaper in heroku

--- config/initializers/infrastructure.rb
+++ config/initializers/infrastructure.rb
@@ -1,7 +1,7 @@
 Ohm.connect :url => ENV["REDISTOGO_URL"]
 
 Rails.application.class.configure do
-  config.event_bus = 'RedisEventBus'
+  config.event_bus = 'InProcessEventBus'
   config.event_subscribers = %w{ClientReport ClientDetailsReport AccountDetailsReport MoneyTransferSaga}
   config.to_prepare { EventBus.init }
 end
