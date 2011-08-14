f4fcd186ef9b15bb8a03d079ee95a53cfffb3660
Luismi Cavalle <luismi@lmcavalle.com>
2011-08-14 10:27:58 +0200

    Remove unused code

--- lib/infrastructure/event_handler.rb
+++ lib/infrastructure/event_handler.rb
@@ -1,22 +1,7 @@
 module EventHandler
   def on(*events, &block)
     events.each do |event_name|
-      ::EventBus.subscribe(event_name) do |event|
-        begin
-          self.handling_event = true
-          block.call(event)
-        ensure
-          self.handling_event = false
-        end
-      end
+      ::EventBus.subscribe(event_name, "#{name}:#{increment_handlers_count}:#{event_name}", &block)
     end
-  end
-
-  def handling_event?
-    Thread.current["handling_event"]
-  end
-
-  def handling_event=(value)
-    Thread.current["handling_event"] = value
   end
 end
